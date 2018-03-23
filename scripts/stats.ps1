d240be83b8a676cd90d140381457926139fd3bd8
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-03-23 09:33:11 -0700

    Stats from mk10 work fine. Now need to add fairness and fighters

---
+++ scripts/stats.ps1
@@ -0,0 +1,81 @@
+<#
+Players
+    + Fighters?
+Pairs
+Chairs
+
+Fairness division across all results
+#>
+
+function Get-MatchResults( $info, $condition, $oposite )
+{
+    $relevantMatches = @($input)
+
+    $isTrue = $relevantMatches | where $condition | measure | % Count
+    $isFalse = $relevantMatches | where $oposite | measure | % Count
+    $isDraw = $relevantMatches | where Winner -eq "draw" | measure | % Count
+
+    "    {0,2} | {1,2} | {2,2}   <{3,2}>  $info" -f $isTrue, $isFalse, $isDraw, $relevantMatches.Count
+}
+
+function Show-Stats( $path = "f:\OneDrive\Projects\Hobbies\Hardware\randomizer\data\sample.csv" )
+{
+    $history = Import-Csv $path
+    $players = $history.FirstPlayer + $history.SecondPlayer | sort -Unique
+
+    ""
+    Write-Host "Players" -fore DarkYellow
+
+    foreach( $player in $players )
+    {
+        $relevantMatches = $history | where{ ($_.FirstPlayer -eq $player) -or ($_.SecondPlayer -eq $player) }
+        $relevantMatches | Get-MatchResults `
+            $player `
+            {$_.Winner -eq $player} `
+            {($_.Winner -ne $player) -and ($_.Winner -ne "draw")}
+    }
+
+    ""
+    Write-Host "Pairs" -fore DarkYellow
+
+    $set = $players
+    foreach( $player in $players )
+    {
+        $set = $set | where{ $psitem -ne $player }
+        foreach( $partner in $set )
+        {
+            $relevantMatches = @($history | where{
+                (($_.FirstPlayer -eq $player) -or ($_.SecondPlayer -eq $player)) -and
+                (($_.FirstPlayer -eq $partner) -or ($_.SecondPlayer -eq $partner)) })
+            $relevantMatches | Get-MatchResults "$player vs $partner" {$_.Winner -eq $player} {$_.Winner -eq $partner}
+        }
+    }
+
+    ""
+    Write-Host "Chairs" -fore DarkYellow
+
+    $history | Get-MatchResults "First chair" {$_.Winner -eq $_.FirstPlayer} {$_.Winner -eq $_.SecondPlayer}
+    foreach( $player in $players )
+    {
+        $relevantMatches = $history | where{ $_.FirstPlayer -eq $player }
+        $info = $relevantMatches | Get-MatchResults `
+            $player `
+            {$_.Winner -eq $player} `
+            {($_.Winner -ne $player) -and ($_.Winner -ne "draw")}
+        " $info"
+    }
+    ""
+
+    $history | Get-MatchResults "Second chair" {$_.Winner -eq $_.SecondPlayer} {$_.Winner -eq $_.FirstPlayer}
+    foreach( $player in $players )
+    {
+        $relevantMatches = $history | where{ $_.SecondPlayer -eq $player }
+        $info = $relevantMatches | Get-MatchResults `
+            $player `
+            {$_.Winner -eq $player} `
+            {($_.Winner -ne $player) -and ($_.Winner -ne "draw")}
+        " $info"
+    }
+    ""
+}
+
