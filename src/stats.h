b94d3a41775d7d0cdd1a25a4e19b1a629b137557
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-03-22 22:31:23 -0700

    Found out memory capacity problem

--- src/stats.h
+++ src/stats.h
@@ -29,21 +29,31 @@ enum Stats
 int winner_selected;
 bool not_fair_win;
 
+// Arduino UNO is at the capacity data-wise.
+// 80 matches is ok since practically we didn't do more than 60.
+// But if there is a way to use Program memory to store this data (place
+// array on stack?) then we should be able to increase the capacity x10.
+//      Device : atmega328p
+//      Program : 7560 bytes(23.1 % Full)(.text +.data +.bootloader)
+//      Data : 1978 bytes(96.6 % Full)(.data +.bss +.noinit)
+const int match_limit = 80;
 int match_current = 0;
-const int match_limit = 1000;
 byte matches[match_limit][Stats::Size];
 
 void RecordMatchOutcome()
 {
     // Record in the stats table
-    matches[match_current][Stats::FirstPlayer] = player_index_first;
-    matches[match_current][Stats::SecondPlayer] = player_index_second;
-    matches[match_current][Stats::Game] = games_index;
-    matches[match_current][Stats::FirstFighter] = fighter_index_first;
-    matches[match_current][Stats::SecondFighter] = fighter_index_second;
-    matches[match_current][Stats::Won] = winner_selected;
-    matches[match_current][Stats::NotFair] = not_fair_win;
-    match_current++;
+    if (match_current < match_limit)
+    {
+        matches[match_current][Stats::FirstPlayer] = player_index_first;
+        matches[match_current][Stats::SecondPlayer] = player_index_second;
+        matches[match_current][Stats::Game] = games_index;
+        matches[match_current][Stats::FirstFighter] = fighter_index_first;
+        matches[match_current][Stats::SecondFighter] = fighter_index_second;
+        matches[match_current][Stats::Won] = winner_selected;
+        matches[match_current][Stats::NotFair] = not_fair_win;
+        match_current++;
+    }
 
     // Output to console
     switch (winner_selected)
@@ -99,7 +109,7 @@ void DumpStats()
         Serial.print(",");
         Serial.print(matches[i][Stats::NotFair]);
         Serial.print(",");
-    Serial.println();
+        Serial.println();
     }
 }
 
