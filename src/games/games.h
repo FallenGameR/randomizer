a296a22d21ee3ad503520b668011df9c34b62a22
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-03-24 21:31:25 -0700

    dice added to arrayof games

--- src/games/games.h
+++ src/games/games.h
@@ -3,15 +3,18 @@
 
 #include "tekken7.h"
 #include "doa5.h"
+#include "dice.h"
 
 // Using PROGMEM here to save some space for variables
 
 const char games_Tekken7[] PROGMEM = "Tekken 7";
 const char games_DeadOrAlive5[] PROGMEM = "Dead or Alive 5";
+const char games_Dice[] PROGMEM = "Dice";
 
 const char *const games[] PROGMEM = {
     games_Tekken7,
     games_DeadOrAlive5,
+    games_Dice,
 };
 
 #define n_games (sizeof(games) / sizeof(const char *))
@@ -21,11 +24,13 @@ int games_index = 0;
 const char *const *fighter_map[] = {
     tekken7,
     doa5,
+    dice,
 };
 
 const byte n_fighter_map[] = {
     n_tekken7,
     n_doa5,
+    n_dice,
 };
 
 const char *const *fighter_map_selected = tekken7;
