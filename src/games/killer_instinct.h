2fa7b5bc40279e65d15ff133fe37ca710c7762fe
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-03-26 19:51:13 -0700

    raw list added

---
+++ src/games/killer_instinct.h
@@ -0,0 +1,29 @@
+Aganos
+Arbiter
+ARIA
+Black Orchid
+Chief Thunder
+Cinder
+Eyedol
+Eagle
+Fulgore
+Gargos
+General RAAM
+Glacius
+Hisako
+Jago
+Kan-Ra
+Kilgore
+Kim Wu
+Maya
+Mira
+Omen
+Rash
+Riptor
+Sabrewulf
+Sadira
+Shadow Jago
+Shin Hisako
+Spinal
+TJ Combo
+Tusk
