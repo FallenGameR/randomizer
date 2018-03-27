4976689c1b4ba48595b1c3460b4364bfeb365d16
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-03-26 23:41:44 -0700

    Games expanded

--- src/games/games.h
+++ src/games/games.h
@@ -3,17 +3,20 @@
 
 #include "tekken7.h"
 #include "doa5.h"
+#include "killer_instinct.h"
 #include "dice.h"
 
 // Using PROGMEM here to save some space for variables
 
 const char games_Tekken7[] PROGMEM = "Tekken 7";
 const char games_DeadOrAlive5[] PROGMEM = "Dead or Alive 5";
+const char games_KillerInstinct[] PROGMEM = "Killer Instinct";
 const char games_Dice[] PROGMEM = "Dice";
 
 const char *const games[] PROGMEM = {
     games_Tekken7,
     games_DeadOrAlive5,
+    games_KillerInstinct,
     games_Dice,
 };
 
@@ -24,12 +27,14 @@ int games_index = 0;
 const char *const *fighter_map[] = {
     tekken7,
     doa5,
+    killer_instinct,
     dice,
 };
 
 const byte n_fighter_map[] = {
     n_tekken7,
     n_doa5,
+    n_killer_instinct,
     n_dice,
 };
 
