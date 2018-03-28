5f63e477c36216803979b6ca7bcc48a879d9166f
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-03-28 09:10:35 -0700

    guilty gear added to games

--- src/games/games.h
+++ src/games/games.h
@@ -3,6 +3,7 @@
 
 #include "tekken7.h"
 #include "doa5.h"
+#include "guilty_gear_xrd_rev2.h"
 #include "killer_instinct.h"
 #include "dice.h"
 
@@ -10,12 +11,14 @@
 
 const char games_Tekken7[] PROGMEM = "Tekken 7";
 const char games_DeadOrAlive5[] PROGMEM = "Dead or Alive 5";
+const char games_GuiltyGearXrdRev2[] PROGMEM = "Guilty Gear Xrd 2";
 const char games_KillerInstinct[] PROGMEM = "Killer Instinct";
 const char games_Dice[] PROGMEM = "Dice";
 
 const char *const games[] PROGMEM = {
     games_Tekken7,
     games_DeadOrAlive5,
+    games_GuiltyGearXrdRev2,
     games_KillerInstinct,
     games_Dice,
 };
@@ -27,6 +30,7 @@ int games_index = 0;
 const char *const *fighter_map[] = {
     tekken7,
     doa5,
+    guilty_gear,
     killer_instinct,
     dice,
 };
@@ -34,6 +38,7 @@ const char *const *fighter_map[] = {
 const byte n_fighter_map[] = {
     n_tekken7,
     n_doa5,
+    n_guilty_gear,
     n_killer_instinct,
     n_dice,
 };
