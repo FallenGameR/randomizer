b4c34202b77deb6e75c4ce1a2069416c6de0caf9
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-03-28 08:56:11 -0700

    killer instinct replacements

--- src/games/killer_instinct.h
+++ src/games/killer_instinct.h
@@ -3,66 +3,66 @@
 
 // Using[] PROGMEM here to save some space for variables
 
-const char killer_instinct_Aganos[] PROGMEM = "Aganos";
-const char killer_instinct_Arbiter[] PROGMEM = "Arbiter";
-const char killer_instinct_ARIA[] PROGMEM = "ARIA";
-const char killer_instinct_BlackOrchid[] PROGMEM = "Black Orchid";
-const char killer_instinct_ChiefThunder[] PROGMEM = "Chief Thunder";
-const char killer_instinct_Cinder[] PROGMEM = "Cinder";
-const char killer_instinct_Eyedol[] PROGMEM = "Eyedol";
-const char killer_instinct_Eagle[] PROGMEM = "Eagle";
-const char killer_instinct_Fulgore[] PROGMEM = "Fulgore";
-const char killer_instinct_Gargos[] PROGMEM = "Gargos";
-const char killer_instinct_GeneralRAAM[] PROGMEM = "General RAAM";
-const char killer_instinct_Glacius[] PROGMEM = "Glacius";
-const char killer_instinct_Hisako[] PROGMEM = "Hisako";
-const char killer_instinct_Jago[] PROGMEM = "Jago";
-const char killer_instinct_KanRa[] PROGMEM = "Kan-Ra";
-const char killer_instinct_Kilgore[] PROGMEM = "Kilgore";
-const char killer_instinct_KimWu[] PROGMEM = "Kim Wu";
-const char killer_instinct_Maya[] PROGMEM = "Maya";
-const char killer_instinct_Mira[] PROGMEM = "Mira";
-const char killer_instinct_Omen[] PROGMEM = "Omen";
-const char killer_instinct_Rash[] PROGMEM = "Rash";
-const char killer_instinct_Riptor[] PROGMEM = "Riptor";
-const char killer_instinct_Sabrewulf[] PROGMEM = "Sabrewulf";
-const char killer_instinct_Sadira[] PROGMEM = "Sadira";
-const char killer_instinct_ShadowJago[] PROGMEM = "Shadow Jago";
-const char killer_instinct_ShinHisako[] PROGMEM = "Shin Hisako";
-const char killer_instinct_Spinal[] PROGMEM = "Spinal";
-const char killer_instinct_TJCombo[] PROGMEM = "TJ Combo";
-const char killer_instinct_Tusk[] PROGMEM = "Tusk";
+const char ki_Aganos[] PROGMEM = "Aganos";
+const char ki_Arbiter[] PROGMEM = "Arbiter";
+const char ki_ARIA[] PROGMEM = "ARIA";
+const char ki_BlackOrchid[] PROGMEM = "Black Orchid";
+const char ki_ChiefThunder[] PROGMEM = "Chief Thunder";
+const char ki_Cinder[] PROGMEM = "Cinder";
+const char ki_Eyedol[] PROGMEM = "Eyedol";
+const char ki_Eagle[] PROGMEM = "Eagle";
+const char ki_Fulgore[] PROGMEM = "Fulgore";
+const char ki_Gargos[] PROGMEM = "Gargos";
+const char ki_GeneralRAAM[] PROGMEM = "General RAAM";
+const char ki_Glacius[] PROGMEM = "Glacius";
+const char ki_Hisako[] PROGMEM = "Hisako";
+const char ki_Jago[] PROGMEM = "Jago";
+const char ki_KanRa[] PROGMEM = "Kan-Ra";
+const char ki_Kilgore[] PROGMEM = "Kilgore";
+const char ki_KimWu[] PROGMEM = "Kim Wu";
+const char ki_Maya[] PROGMEM = "Maya";
+const char ki_Mira[] PROGMEM = "Mira";
+const char ki_Omen[] PROGMEM = "Omen";
+const char ki_Rash[] PROGMEM = "Rash";
+const char ki_Riptor[] PROGMEM = "Riptor";
+const char ki_Sabrewulf[] PROGMEM = "Sabrewulf";
+const char ki_Sadira[] PROGMEM = "Sadira";
+const char ki_ShadowJago[] PROGMEM = "Shadow Jago";
+const char ki_ShinHisako[] PROGMEM = "Shin Hisako";
+const char ki_Spinal[] PROGMEM = "Spinal";
+const char ki_TJCombo[] PROGMEM = "TJ Combo";
+const char ki_Tusk[] PROGMEM = "Tusk";
 
 const char *const killer_instinct[] PROGMEM = {
-    killer_instinct_Aganos,
-    killer_instinct_Arbiter,
-    killer_instinct_ARIA,
-    killer_instinct_BlackOrchid,
-    killer_instinct_ChiefThunder,
-    killer_instinct_Cinder,
-    killer_instinct_Eyedol,
-    killer_instinct_Eagle,
-    killer_instinct_Fulgore,
-    killer_instinct_Gargos,
-    killer_instinct_GeneralRAAM,
-    killer_instinct_Glacius,
-    killer_instinct_Hisako,
-    killer_instinct_Jago,
-    killer_instinct_KanRa,
-    killer_instinct_Kilgore,
-    killer_instinct_KimWu,
-    killer_instinct_Maya,
-    killer_instinct_Mira,
-    killer_instinct_Omen,
-    killer_instinct_Rash,
-    killer_instinct_Riptor,
-    killer_instinct_Sabrewulf,
-    killer_instinct_Sadira,
-    killer_instinct_ShadowJago,
-    killer_instinct_ShinHisako,
-    killer_instinct_Spinal,
-    killer_instinct_TJCombo,
-    killer_instinct_Tusk,
+    ki_Aganos,
+    ki_Arbiter,
+    ki_ARIA,
+    ki_BlackOrchid,
+    ki_ChiefThunder,
+    ki_Cinder,
+    ki_Eyedol,
+    ki_Eagle,
+    ki_Fulgore,
+    ki_Gargos,
+    ki_GeneralRAAM,
+    ki_Glacius,
+    ki_Hisako,
+    ki_Jago,
+    ki_KanRa,
+    ki_Kilgore,
+    ki_KimWu,
+    ki_Maya,
+    ki_Mira,
+    ki_Omen,
+    ki_Rash,
+    ki_Riptor,
+    ki_Sabrewulf,
+    ki_Sadira,
+    ki_ShadowJago,
+    ki_ShinHisako,
+    ki_Spinal,
+    ki_TJCombo,
+    ki_Tusk,
 };
 
 #define n_killer_instinct (sizeof(killer_instinct) / sizeof(const char *))
