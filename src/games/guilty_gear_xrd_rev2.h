b78b99a9108c2c5546261bb41470cb2f15d559fb
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-03-28 09:07:43 -0700

    fighters in PRGMEM form

--- src/games/guilty_gear_xrd_rev2.h
+++ src/games/guilty_gear_xrd_rev2.h
@@ -3,33 +3,29 @@
 
 // Using[] PROGMEM here to save some space for variables
 
-const char ggx_Aganos[] PROGMEM = "Aganos";
-
-/*
-Sol Badguy
-Ky Kiske
-Millia Rage
-Zato-1
-May
-Potemkin
-Chipp Zanuff
-Venom
-Axl Low
-I-No
-Faust
-Slayer
-Ramlethal
-Bedman
-Sin Kiske
-Elphelt
-Leo Whitefang
-Johnny
-Jack-O Valentine
-Jam Kuradoberi
-Raven
-Kum Haehyun
-Dizzy
-/**/
+const char ggx_SolBadguy[] PROGMEM = "Sol Badguy";
+const char ggx_KyKiske[] PROGMEM = "Ky Kiske";
+const char ggx_MilliaRage[] PROGMEM = "Millia Rage";
+const char ggx_Zato1[] PROGMEM = "Zato-1";
+const char ggx_May[] PROGMEM = "May";
+const char ggx_Potemkin[] PROGMEM = "Potemkin";
+const char ggx_ChippZanuff[] PROGMEM = "Chipp Zanuff";
+const char ggx_Venom[] PROGMEM = "Venom";
+const char ggx_AxlLow[] PROGMEM = "Axl Low";
+const char ggx_INo[] PROGMEM = "I-No";
+const char ggx_Faust[] PROGMEM = "Faust";
+const char ggx_Slayer[] PROGMEM = "Slayer";
+const char ggx_Ramlethal[] PROGMEM = "Ramlethal";
+const char ggx_Bedman[] PROGMEM = "Bedman";
+const char ggx_SinKiske[] PROGMEM = "Sin Kiske";
+const char ggx_Elphelt[] PROGMEM = "Elphelt";
+const char ggx_LeoWhitefang[] PROGMEM = "Leo Whitefang";
+const char ggx_Johnny[] PROGMEM = "Johnny";
+const char ggx_JackOValentine[] PROGMEM = "Jack-O Valentine";
+const char ggx_JamKuradoberi[] PROGMEM = "Jam Kuradoberi";
+const char ggx_Raven[] PROGMEM = "Raven";
+const char ggx_KumHaehyun[] PROGMEM = "Kum Haehyun";
+const char ggx_Dizzy[] PROGMEM = "Dizzy";
 
 const char *const guilty_gear[] PROGMEM = {
     ki_Aganos,
