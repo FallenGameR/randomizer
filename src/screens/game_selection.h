fee1c0050348e922026f07df50e6167307eeee89
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-04-01 22:54:13 -0700

    Tag selection added

--- src/screens/game_selection.h
+++ src/screens/game_selection.h
@@ -17,6 +17,18 @@ void GameSelectionScreen()
         LCD_PRINT(games, games_index);
         SERIAL_PRINT_LN(games, games_index);
 
+        if (t_fighter_map_selected == 0)
+        {
+            isTagGame = false;
+        }
+
+        if (isTagGame)
+        {
+            lcd.setCursor(0, 1);
+            lcd.print(F("Tag"));
+            Serial.println(F("Tag"));
+        }
+
         screen_redraw = false;
     }
 
@@ -27,6 +39,7 @@ void GameSelectionScreen()
             games_index = (games_index + n_games + 1) % n_games;
             fighter_map_selected = fighter_map[games_index];
             n_fighter_map_selected = n_fighter_map[games_index];
+            t_fighter_map_selected = t_fighter_map[games_index];
             input_allowed = false;
             screen_redraw = true;
         }
@@ -36,6 +49,18 @@ void GameSelectionScreen()
             games_index = (games_index + n_games - 1) % n_games;
             fighter_map_selected = fighter_map[games_index];
             n_fighter_map_selected = n_fighter_map[games_index];
+            t_fighter_map_selected = t_fighter_map[games_index];
+            input_allowed = false;
+            screen_redraw = true;
+        }
+
+        if (Y_UP || Y_DOWN)
+        {
+            if (t_fighter_map_selected > 0)
+            {
+                isTagGame = !isTagGame;
+            }
+
             input_allowed = false;
             screen_redraw = true;
         }
