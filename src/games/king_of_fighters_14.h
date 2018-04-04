49be926202b23dc7893359989f5e6c4963a6dba7
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-04-03 21:51:51 -0700

    kof14 array added

--- src/games/king_of_fighters_14.h
+++ src/games/king_of_fighters_14.h
@@ -3,10 +3,6 @@
 
 // Using PROGMEM here to save some space for variables
 
-/*
-
-const char doa5_AkiraYuki[] PROGMEM = "Akira Yuki";
-
 // Japan Team
 const char kof14_KyoKusanagi[] PROGMEM = "Kyo Kusanagi";
 const char kof14_BenimaruNikaido[] PROGMEM = "Benimaru Nikaido";
@@ -81,10 +77,79 @@ const char kof14_Vanessa[] PROGMEM = "Vanessa";
 const char kof14_RockHoward[] PROGMEM = "Rock Howard";
 
 const char *const kof14[] PROGMEM = {
-    doa5_AkiraYuki,
+    // Japan Team
+    kof14_KyoKusanagi,
+    kof14_BenimaruNikaido,
+    kof14_GoroDaimon,
+    // Yagami Team
+    kof14_IoriYagami,
+    kof14_Mature,
+    kof14_Vice,
+    // Fatal Fury Team
+    kof14_TerryBogard,
+    kof14_AndyBogard,
+    kof14_JoeHigashi,
+    // Kim Team
+    kof14_KimKaphwan,
+    kof14_GangIl,
+    kof14_Luong,
+    // Villains Team
+    kof14_Xanadu,
+    kof14_ChangKoehan,
+    kof14_ChoiBounge,
+    // Official Invitation Team
+    kof14_SylviePaula,
+    kof14_Kukri,
+    kof14_Mian,
+    // South America Team
+    kof14_Nelson,
+    kof14_Zarina,
+    kof14_BandeirasHattori,
+    // Mexico Team
+    kof14_Ramon,
+    kof14_Angel,
+    kof14_KingOfDino,
+    // South Town Team
+    kof14_GeeseHoward,
+    kof14_BillyKane,
+    kof14_Hein,
+    // China Team
+    kof14_ShunEi,
+    kof14_TungFuRue,
+    kof14_Meitenkun,
+    // Ikari Warriors Team
+    kof14_RalfJones,
+    kof14_ClarkStill,
+    kof14_LeonaHeidern,
+    // K' Team
+    kof14_K,
+    kof14_KulaDiamond,
+    kof14_Maxima,
+    // Women Fighters Team
+    kof14_King,
+    kof14_MaiShiranui,
+    kof14_Alice,
+    // Psycho Soldier Team
+    kof14_AthenaAsamiya,
+    kof14_SieKensou,
+    kof14_ChinGentsai,
+    // Art of Fighting Team
+    kof14_RyoSakazaki,
+    kof14_RobertGarcia,
+    kof14_YuriSakazaki,
+    // Another World Team
+    kof14_Nakoruru,
+    kof14_MuiMui,
+    kof14_LoveHeart,
+    // Antonov Team
+    kof14_RyujiYamazaki,
+    kof14_Whip,
+    kof14_Antonov,
+    // Verse Team
+    kof14_Verse,
+    kof14_Vanessa,
+    kof14_RockHoward,
 };
-
-/**/
 
 #define n_kof14 (sizeof(kof14) / sizeof(const char *))
 
