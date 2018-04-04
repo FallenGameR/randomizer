27350057c95739e36ee1fab989df30f013aed0de
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-04-03 21:26:15 -0700

    kof started, needs validation

---
+++ src/games/king_of_fighters_14.h
@@ -0,0 +1,80 @@
+/*
+Japan Team
+    Kyo Kusanagi
+    Benimaru Nikaido
+    Goro Daimon
+Yagami Team
+    Iori Yagami
+    Mature
+    Vice
+Fatal Fury Team
+    Terry Bogard
+    Andy Bogard
+    Joe Higashi
+Art of Fighting Team
+    Ryo Sakazaki
+    Robert Garcia
+    Yuri Sakazaki
+Kim Team
+    Kim Kaphwan
+    Gang-il
+    Luong
+Villains Team
+    Xanadu
+    Chang Koehan
+    Choi Bounge
+Official Invitation Team
+    Sylvie Paula Paula
+    Kukri
+    Mian
+South America Team
+    Nelson
+    Zarina
+    Bandeiras Hattori
+Mexico Team
+    Ramon
+    Angel
+    King of Dinosaurs
+South Town Team
+    Geese Howard
+    Billy Kane
+    Hein
+China Team
+    Shun'ei
+    Tung Fu Rue
+    Meitenkun
+Psycho Soldier Team
+    Athena Asamiya
+    Sie Kensou
+    Chin Gentsai
+Ikari Warriors Team
+    Leona Heidern
+    Ralf Jones
+    Clark Still
+Another World Team
+    Nakoruru
+    Mui Mui
+    Love Heart
+K' Team
+    K'
+    Kula Diamond
+    Maxima
+Women Fighters Team
+    King
+    Mai Shiranui
+    Alice
+Bosses
+    Antonov
+    Verse
+DLCEdit
+Wave 1Edit
+    Whip
+    Ryuji Yamazaki
+    Vanessa
+    Rock Howard
+Wave 2Edit
+    Oswald
+    Heidern
+    Najd
+
+/**/
