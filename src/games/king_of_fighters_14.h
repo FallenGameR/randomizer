76c5add58d10225b0b6da3c845067c2cc4731706
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-04-03 21:44:22 -0700

    Preparing annotation

--- src/games/king_of_fighters_14.h
+++ src/games/king_of_fighters_14.h
@@ -7,75 +7,75 @@
 
 const char doa5_AkiraYuki[] PROGMEM = "Akira Yuki";
 
-Japan Team
+// Japan Team
     Kyo Kusanagi
     Benimaru Nikaido
     Goro Daimon
-Yagami Team
+// Yagami Team
     Iori Yagami
     Mature
     Vice
-Fatal Fury Team
+// Fatal Fury Team
     Terry Bogard
     Andy Bogard
     Joe Higashi
-Kim Team
+// Kim Team
     Kim Kaphwan
     Gang-il
     Luong
-Villains Team
+// Villains Team
     Xanadu
     Chang Koehan
     Choi Bounge    
-Official Invitation Team
+// Official Invitation Team
     Sylvie Paula
     Kukri
     Mian
-South America Team
+// South America Team
     Nelson
     Zarina
     Bandeiras Hattori
-Mexico Team
+// Mexico Team
     Ramon
     Angel
     King of Dino
-South Town Team
+// South Town Team
     Geese Howard
     Billy Kane
     Hein
-China Team
+// China Team
     Shun'ei
     Tung Fu Rue
     Meitenkun
-Ikari Warriors Team
+// Ikari Warriors Team
     Ralf Jones
     Clark Still
     Leona Heidern
-K' Team
+// K' Team
     K'
     Kula Diamond
     Maxima
-Women Fighters Team
+// Women Fighters Team
     King
     Mai Shiranui
     Alice
-Psycho Soldier Team
+// Psycho Soldier Team
     Athena Asamiya
     Sie Kensou
     Chin Gentsai
-Art of Fighting Team
+// Art of Fighting Team
     Ryo Sakazaki
     Robert Garcia
     Yuri Sakazaki
-Another World Team
+// Another World Team
     Nakoruru
     Mui Mui
     Love Heart
-Antonov Team
+// Antonov Team
     Ryuji Yamazaki
     Whip
     Antonov
-Verse Team
+// Verse Team
     Verse
     Vanessa
     Rock Howard
