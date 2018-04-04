894f8d8bc4cdfa00fc1fa95c03032733abbd5454
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-04-03 21:50:15 -0700

    extra spaces removed

--- src/games/king_of_fighters_14.h
+++ src/games/king_of_fighters_14.h
@@ -8,77 +8,77 @@
 const char doa5_AkiraYuki[] PROGMEM = "Akira Yuki";
 
 // Japan Team
-const char kof14_KyoKusanagi [] PROGMEM = "Kyo Kusanagi";
-const char kof14_BenimaruNikaido [] PROGMEM = "Benimaru Nikaido";
-const char kof14_GoroDaimon [] PROGMEM = "Goro Daimon";
+const char kof14_KyoKusanagi[] PROGMEM = "Kyo Kusanagi";
+const char kof14_BenimaruNikaido[] PROGMEM = "Benimaru Nikaido";
+const char kof14_GoroDaimon[] PROGMEM = "Goro Daimon";
 // Yagami Team
-const char kof14_IoriYagami [] PROGMEM = "Iori Yagami";
-const char kof14_Mature [] PROGMEM = "Mature";
-const char kof14_Vice [] PROGMEM = "Vice";
+const char kof14_IoriYagami[] PROGMEM = "Iori Yagami";
+const char kof14_Mature[] PROGMEM = "Mature";
+const char kof14_Vice[] PROGMEM = "Vice";
 // Fatal Fury Team
-const char kof14_TerryBogard [] PROGMEM = "Terry Bogard";
-const char kof14_AndyBogard [] PROGMEM = "Andy Bogard";
-const char kof14_JoeHigashi [] PROGMEM = "Joe Higashi";
+const char kof14_TerryBogard[] PROGMEM = "Terry Bogard";
+const char kof14_AndyBogard[] PROGMEM = "Andy Bogard";
+const char kof14_JoeHigashi[] PROGMEM = "Joe Higashi";
 // Kim Team
-const char kof14_KimKaphwan [] PROGMEM = "Kim Kaphwan";
-const char kof14_GangIl [] PROGMEM = "Gang-il";
-const char kof14_Luong [] PROGMEM = "Luong";
+const char kof14_KimKaphwan[] PROGMEM = "Kim Kaphwan";
+const char kof14_GangIl[] PROGMEM = "Gang-il";
+const char kof14_Luong[] PROGMEM = "Luong";
 // Villains Team
-const char kof14_Xanadu [] PROGMEM = "Xanadu";
-const char kof14_ChangKoehan [] PROGMEM = "Chang Koehan";
-const char kof14_ChoiBounge     [] PROGMEM = "Choi Bounge    ";
+const char kof14_Xanadu[] PROGMEM = "Xanadu";
+const char kof14_ChangKoehan[] PROGMEM = "Chang Koehan";
+const char kof14_ChoiBounge[] PROGMEM = "Choi Bounge    ";
 // Official Invitation Team
-const char kof14_SylviePaula [] PROGMEM = "Sylvie Paula";
-const char kof14_Kukri [] PROGMEM = "Kukri";
-const char kof14_Mian [] PROGMEM = "Mian";
+const char kof14_SylviePaula[] PROGMEM = "Sylvie Paula";
+const char kof14_Kukri[] PROGMEM = "Kukri";
+const char kof14_Mian[] PROGMEM = "Mian";
 // South America Team
-const char kof14_Nelson [] PROGMEM = "Nelson";
-const char kof14_Zarina [] PROGMEM = "Zarina";
-const char kof14_BandeirasHattori [] PROGMEM = "Bandeiras Hattori";
+const char kof14_Nelson[] PROGMEM = "Nelson";
+const char kof14_Zarina[] PROGMEM = "Zarina";
+const char kof14_BandeirasHattori[] PROGMEM = "Bandeiras Hattori";
 // Mexico Team
-const char kof14_Ramon [] PROGMEM = "Ramon";
-const char kof14_Angel [] PROGMEM = "Angel";
-const char kof14_KingOfDino [] PROGMEM = "King of Dino";
+const char kof14_Ramon[] PROGMEM = "Ramon";
+const char kof14_Angel[] PROGMEM = "Angel";
+const char kof14_KingOfDino[] PROGMEM = "King of Dino";
 // South Town Team
-const char kof14_GeeseHoward [] PROGMEM = "Geese Howard";
-const char kof14_BillyKane [] PROGMEM = "Billy Kane";
-const char kof14_Hein [] PROGMEM = "Hein";
+const char kof14_GeeseHoward[] PROGMEM = "Geese Howard";
+const char kof14_BillyKane[] PROGMEM = "Billy Kane";
+const char kof14_Hein[] PROGMEM = "Hein";
 // China Team
-const char kof14_ShunEi [] PROGMEM = "Shun'ei";
-const char kof14_TungFuRue [] PROGMEM = "Tung Fu Rue";
-const char kof14_Meitenkun [] PROGMEM = "Meitenkun";
+const char kof14_ShunEi[] PROGMEM = "Shun'ei";
+const char kof14_TungFuRue[] PROGMEM = "Tung Fu Rue";
+const char kof14_Meitenkun[] PROGMEM = "Meitenkun";
 // Ikari Warriors Team
-const char kof14_RalfJones [] PROGMEM = "Ralf Jones";
-const char kof14_ClarkStill [] PROGMEM = "Clark Still";
-const char kof14_LeonaHeidern [] PROGMEM = "Leona Heidern";
+const char kof14_RalfJones[] PROGMEM = "Ralf Jones";
+const char kof14_ClarkStill[] PROGMEM = "Clark Still";
+const char kof14_LeonaHeidern[] PROGMEM = "Leona Heidern";
 // K' Team
-const char kof14_K [] PROGMEM = "K'";
-const char kof14_KulaDiamond [] PROGMEM = "Kula Diamond";
-const char kof14_Maxima [] PROGMEM = "Maxima";
+const char kof14_K[] PROGMEM = "K'";
+const char kof14_KulaDiamond[] PROGMEM = "Kula Diamond";
+const char kof14_Maxima[] PROGMEM = "Maxima";
 // Women Fighters Team
-const char kof14_King [] PROGMEM = "King";
-const char kof14_MaiShiranui [] PROGMEM = "Mai Shiranui";
-const char kof14_Alice [] PROGMEM = "Alice";
+const char kof14_King[] PROGMEM = "King";
+const char kof14_MaiShiranui[] PROGMEM = "Mai Shiranui";
+const char kof14_Alice[] PROGMEM = "Alice";
 // Psycho Soldier Team
-const char kof14_AthenaAsamiya [] PROGMEM = "Athena Asamiya";
-const char kof14_SieKensou [] PROGMEM = "Sie Kensou";
-const char kof14_ChinGentsai [] PROGMEM = "Chin Gentsai";
+const char kof14_AthenaAsamiya[] PROGMEM = "Athena Asamiya";
+const char kof14_SieKensou[] PROGMEM = "Sie Kensou";
+const char kof14_ChinGentsai[] PROGMEM = "Chin Gentsai";
 // Art of Fighting Team
-const char kof14_RyoSakazaki [] PROGMEM = "Ryo Sakazaki";
-const char kof14_RobertGarcia [] PROGMEM = "Robert Garcia";
-const char kof14_YuriSakazaki [] PROGMEM = "Yuri Sakazaki";
+const char kof14_RyoSakazaki[] PROGMEM = "Ryo Sakazaki";
+const char kof14_RobertGarcia[] PROGMEM = "Robert Garcia";
+const char kof14_YuriSakazaki[] PROGMEM = "Yuri Sakazaki";
 // Another World Team
-const char kof14_Nakoruru [] PROGMEM = "Nakoruru";
-const char kof14_MuiMui [] PROGMEM = "Mui Mui";
-const char kof14_LoveHeart [] PROGMEM = "Love Heart";
+const char kof14_Nakoruru[] PROGMEM = "Nakoruru";
+const char kof14_MuiMui[] PROGMEM = "Mui Mui";
+const char kof14_LoveHeart[] PROGMEM = "Love Heart";
 // Antonov Team
-const char kof14_RyujiYamazaki [] PROGMEM = "Ryuji Yamazaki";
-const char kof14_Whip [] PROGMEM = "Whip";
-const char kof14_Antonov [] PROGMEM = "Antonov";
+const char kof14_RyujiYamazaki[] PROGMEM = "Ryuji Yamazaki";
+const char kof14_Whip[] PROGMEM = "Whip";
+const char kof14_Antonov[] PROGMEM = "Antonov";
 // Verse Team
-const char kof14_Verse [] PROGMEM = "Verse";
-const char kof14_Vanessa [] PROGMEM = "Vanessa";
-const char kof14_RockHoward [] PROGMEM = "Rock Howard";
+const char kof14_Verse[] PROGMEM = "Verse";
+const char kof14_Vanessa[] PROGMEM = "Vanessa";
+const char kof14_RockHoward[] PROGMEM = "Rock Howard";
 
 const char *const kof14[] PROGMEM = {
     doa5_AkiraYuki,
