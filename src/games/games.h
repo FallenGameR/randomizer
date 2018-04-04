60ce7f126f4378bedf179de6b9da704de8b49f1f
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-04-03 21:54:01 -0700

    kof added to games

--- src/games/games.h
+++ src/games/games.h
@@ -4,6 +4,7 @@
 #include "tekken7.h"
 #include "doa5.h"
 #include "guilty_gear_xrd_rev2.h"
+#include "king_of_fighters_14.h"
 #include "killer_instinct.h"
 #include "dice.h"
 
@@ -12,6 +13,7 @@
 const char games_Tekken7[] PROGMEM = "Tekken 7";
 const char games_DeadOrAlive5[] PROGMEM = "Dead or Alive 5";
 const char games_GuiltyGearXrdRev2[] PROGMEM = "Guilty Gear Xrd";
+const char games_KingOfFighters14[] PROGMEM = "King of Fighters";
 const char games_KillerInstinct[] PROGMEM = "Killer Instinct";
 const char games_Dice[] PROGMEM = "Dice";
 
@@ -19,6 +21,7 @@ const char *const games[] PROGMEM = {
     games_Tekken7,
     games_DeadOrAlive5,
     games_GuiltyGearXrdRev2,
+    games_KingOfFighters14,
     games_KillerInstinct,
     games_Dice,
 };
@@ -32,6 +35,7 @@ const char *const *fighter_map[] = {
     tekken7,
     doa5,
     guilty_gear,
+    kof14,
     killer_instinct,
     dice,
 };
@@ -41,6 +45,7 @@ const byte n_fighter_map[] = {
     n_tekken7,
     n_doa5,
     n_guilty_gear,
+    n_kof14,
     n_killer_instinct,
     n_dice,
 };
@@ -50,6 +55,7 @@ const byte t_fighter_map[] = {
     0, // no tag matches in Tekken
     2, // 2 fighters in tag match in Dead or Alive
     0, // no tag matches in Guilty Gear
+    3, // 3 fighters teams in King of Fighters
     0, // no tag matches in Killer Instinct
     0, // no tag matches in Dice
 };
