a0459d8e5ee847386e62e9fdd40360d6c2990a0b
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-05-13 23:19:24 -0700

    normalized fighters

--- src/games/mortal_kombat_xl.h
+++ src/games/mortal_kombat_xl.h
@@ -1,68 +1,37 @@
 /*
 
- Alien in Mortal Kombat XL Alien
- Bo' Rai Cho in Mortal Kombat XL Bo' Rai Cho
- Cassie Cage in Mortal Kombat XL Cassie Cage
-Moves
- D'Vorah in Mortal Kombat XL D'Vorah
-Moves
- Ermac in Mortal Kombat XL Ermac
-Moves
- Erron Black in Mortal Kombat XL Erron Black
-Moves
- Ferra/Torr in Mortal Kombat XL Ferra/Torr
-Moves
- Goro in Mortal Kombat XL Goro
-Moves
- Jacqui Briggs in Mortal Kombat XL Jacqui Briggs
-Moves
- Jason in Mortal Kombat XL Jason
-Moves
- Jax in Mortal Kombat XL Jax
-Moves
- Johnny Cage in Mortal Kombat XL Johnny Cage
-Moves
- Kano in Mortal Kombat XL Kano
-Moves
- Kenshi in Mortal Kombat XL Kenshi
-Moves
- Kitana in Mortal Kombat XL Kitana
-Moves
- Kotal Kahn in Mortal Kombat XL Kotal Kahn
-Moves
- Kung Jin in Mortal Kombat XL Kung Jin
-Moves
- Kung Lao in Mortal Kombat XL Kung Lao
-Moves
- Leatherface in Mortal Kombat XL Leatherface
-Moves
- Liu Kang in Mortal Kombat XL Liu Kang
-Moves
- Mileena in Mortal Kombat XL Mileena
-Moves
- Predator in Mortal Kombat XL Predator
-Moves
- Quan Chi in Mortal Kombat XL Quan Chi
-Moves
- Raiden in Mortal Kombat XL Raiden
-Moves
- Reptile in Mortal Kombat XL Reptile
-Moves
- Scorpion in Mortal Kombat XL Scorpion
-Moves
- Shinnok in Mortal Kombat XL Shinnok
-Moves
- Sonya in Mortal Kombat XL Sonya
-Moves
- Sub-Zero in Mortal Kombat XL Sub-Zero
-Moves
- Takeda in Mortal Kombat XL Takeda
-Moves
- Tanya in Mortal Kombat XL Tanya
-Moves
- Tremor in Mortal Kombat XL Tremor
-Moves
- Triborg in Mortal Kombat XL Triborg
-Moves
+Alien
+Bo'Rai Cho
+Cassie Cage
+D'Vorah
+Ermac
+Erron Black
+Ferra/Torr
+Goro
+Jacqui Briggs
+Jason
+Jax
+Johnny Cage
+Kano
+Kenshi
+Kitana
+Kotal Kahn
+Kung Jin
+Kung Lao
+Leatherface
+Liu Kang
+Mileena
+Predator
+Quan Chi
+Raiden
+Reptile
+Scorpion
+Shinnok
+Sonya
+Sub-Zero
+Takeda
+Tanya
+Tremor
+Triborg
 
 /**/
