21e43edd3666ab7a5e0b04883470c214abe25142
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-05-13 23:14:50 -0700

    Draft population of mkx and sf5

---
+++ src/games/mortal_kombat_xl.h
@@ -0,0 +1,68 @@
+/*
+
+ Alien in Mortal Kombat XL Alien
+ Bo' Rai Cho in Mortal Kombat XL Bo' Rai Cho
+ Cassie Cage in Mortal Kombat XL Cassie Cage
+Moves
+ D'Vorah in Mortal Kombat XL D'Vorah
+Moves
+ Ermac in Mortal Kombat XL Ermac
+Moves
+ Erron Black in Mortal Kombat XL Erron Black
+Moves
+ Ferra/Torr in Mortal Kombat XL Ferra/Torr
+Moves
+ Goro in Mortal Kombat XL Goro
+Moves
+ Jacqui Briggs in Mortal Kombat XL Jacqui Briggs
+Moves
+ Jason in Mortal Kombat XL Jason
+Moves
+ Jax in Mortal Kombat XL Jax
+Moves
+ Johnny Cage in Mortal Kombat XL Johnny Cage
+Moves
+ Kano in Mortal Kombat XL Kano
+Moves
+ Kenshi in Mortal Kombat XL Kenshi
+Moves
+ Kitana in Mortal Kombat XL Kitana
+Moves
+ Kotal Kahn in Mortal Kombat XL Kotal Kahn
+Moves
+ Kung Jin in Mortal Kombat XL Kung Jin
+Moves
+ Kung Lao in Mortal Kombat XL Kung Lao
+Moves
+ Leatherface in Mortal Kombat XL Leatherface
+Moves
+ Liu Kang in Mortal Kombat XL Liu Kang
+Moves
+ Mileena in Mortal Kombat XL Mileena
+Moves
+ Predator in Mortal Kombat XL Predator
+Moves
+ Quan Chi in Mortal Kombat XL Quan Chi
+Moves
+ Raiden in Mortal Kombat XL Raiden
+Moves
+ Reptile in Mortal Kombat XL Reptile
+Moves
+ Scorpion in Mortal Kombat XL Scorpion
+Moves
+ Shinnok in Mortal Kombat XL Shinnok
+Moves
+ Sonya in Mortal Kombat XL Sonya
+Moves
+ Sub-Zero in Mortal Kombat XL Sub-Zero
+Moves
+ Takeda in Mortal Kombat XL Takeda
+Moves
+ Tanya in Mortal Kombat XL Tanya
+Moves
+ Tremor in Mortal Kombat XL Tremor
+Moves
+ Triborg in Mortal Kombat XL Triborg
+Moves
+
+/**/
---
+++ src/games/street_fighter_5.h
@@ -0,0 +1,38 @@
+/*
+
+Abigail	Final Fight (boss), first Street Fighter appearance
+Akuma	Super Street Fighter II Turbo
+Alex	Street Fighter III: New Generation
+Balrog	Street Fighter II
+Birdie	Street Fighter
+//Blanka *	Street Fighter II
+Cammy	Super Street Fighter II
+Chun-Li	Street Fighter II
+//Cody *	Final Fight / Street Fighter Alpha 3
+Dhalsim	Street Fighter II
+Ed	Street Fighter IV (NPC)
+F.A.N.G	Street Fighter V
+//Falke *	First appearance
+//G *	First appearance
+Guile	Street Fighter II
+Ibuki	Street Fighter III: New Generation
+Juri	Super Street Fighter IV
+Karin	Street Fighter Alpha 3
+Ken	Street Fighter
+Kolin	Street Fighter III: New Generation (NPC)
+Laura	Street Fighter V
+Bison	Street Fighter II
+Menat	Street Fighter V
+Nash	Street Fighter Alpha
+Necalli	Street Fighter V
+R. Mika	Street Fighter Alpha 3
+Rashid	Street Fighter V
+Ryu	Street Fighter
+//Sagat *	Street Fighter
+//Sakura *	Street Fighter Alpha 2
+Urien	Street Fighter III: 2nd Impact
+Vega	Street Fighter II
+Zangief	Street Fighter II
+Zeku	Street Fighter Alpha 2 (NPC)
+
+/**/
