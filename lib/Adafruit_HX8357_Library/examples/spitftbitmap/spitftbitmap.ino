f90ac63b0fb9f816264f4d465502cfeb2747d878
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-05-25 22:32:00 -0700

    Switched to mega, added new display from sample code.

--- lib/Adafruit_HX8357_Library/examples/spitftbitmap/spitftbitmap.ino
+++ lib/Adafruit_HX8357_Library/examples/spitftbitmap/spitftbitmap.ino
@@ -1,5 +1,3 @@
-#include <Arduino.h>
-
 /***************************************************
   This is our Bitmap drawing example for the Adafruit HX8357 Breakout
   ----> http://www.adafruit.com/products/2050
@@ -15,8 +13,9 @@
   MIT license, all text above must be included in any redistribution
  ****************************************************/
 
-#include <Adafruit_GFX.h> // Core graphics library
-#include <Adafruit_HX8357.h>
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include "Adafruit_HX8357.h"
 #include <SPI.h>
 #include <SD.h>
 
@@ -32,29 +31,22 @@ Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC);
 
 #define SD_CS 4
 
-void bmpDraw(char *filename, uint8_t x, uint16_t y);
-uint16_t read16(File &f);
-uint32_t read32(File &f);
-
-void setup(void)
-{
+void setup(void) {
   Serial.begin(9600);
 
+  tft.begin(HX8357D);
+  tft.fillScreen(HX8357_BLUE);
+  
   Serial.print("Initializing SD card...");
-  if (!SD.begin(SD_CS))
-  {
+  if (!SD.begin(SD_CS)) {
     Serial.println("failed!");
   }
   Serial.println("OK!");
 
-  tft.begin(HX8357D);
-  tft.fillScreen(HX8357_BLUE);
-
   bmpDraw("jumpers.bmp", 0, 0);
 }
 
-void loop()
-{
+void loop() {
 }
 
 // This function opens a Windows Bitmap (BMP) file and
@@ -67,24 +59,22 @@ void loop()
 
 #define BUFFPIXEL 20
 
-void bmpDraw(char *filename, uint8_t x, uint16_t y)
-{
+void bmpDraw(char *filename, uint8_t x, uint16_t y) {
 
-  File bmpFile;
-  int bmpWidth, bmpHeight;            // W+H in pixels
-  uint8_t bmpDepth;                   // Bit depth (currently must be 24)
-  uint32_t bmpImageoffset;            // Start of image data in file
-  uint32_t rowSize;                   // Not always = bmpWidth; may have padding
-  uint8_t sdbuffer[3 * BUFFPIXEL];    // pixel buffer (R+G+B per pixel)
-  uint8_t buffidx = sizeof(sdbuffer); // Current position in sdbuffer
-  boolean goodBmp = false;            // Set to true on valid header parse
-  boolean flip = true;                // BMP is stored bottom-to-top
-  int w, h, row, col;
-  uint8_t r, g, b;
+  File     bmpFile;
+  int      bmpWidth, bmpHeight;   // W+H in pixels
+  uint8_t  bmpDepth;              // Bit depth (currently must be 24)
+  uint32_t bmpImageoffset;        // Start of image data in file
+  uint32_t rowSize;               // Not always = bmpWidth; may have padding
+  uint8_t  sdbuffer[3*BUFFPIXEL]; // pixel buffer (R+G+B per pixel)
+  uint8_t  buffidx = sizeof(sdbuffer); // Current position in sdbuffer
+  boolean  goodBmp = false;       // Set to true on valid header parse
+  boolean  flip    = true;        // BMP is stored bottom-to-top
+  int      w, h, row, col;
+  uint8_t  r, g, b;
   uint32_t pos = 0, startTime = millis();
 
-  if ((x >= tft.width()) || (y >= tft.height()))
-    return;
+  if((x >= tft.width()) || (y >= tft.height())) return;
 
   Serial.println();
   Serial.print(F("Loading image '"));
@@ -92,33 +82,25 @@ void bmpDraw(char *filename, uint8_t x, uint16_t y)
   Serial.println('\'');
 
   // Open requested file on SD card
-  if ((bmpFile = SD.open(filename)) == NULL)
-  {
+  if ((bmpFile = SD.open(filename)) == NULL) {
     Serial.print(F("File not found"));
     return;
   }
 
   // Parse BMP header
-  if (read16(bmpFile) == 0x4D42)
-  { // BMP signature
-    Serial.print(F("File size: "));
-    Serial.println(read32(bmpFile));
-    (void)read32(bmpFile);            // Read & ignore creator bytes
+  if(read16(bmpFile) == 0x4D42) { // BMP signature
+    Serial.print(F("File size: ")); Serial.println(read32(bmpFile));
+    (void)read32(bmpFile); // Read & ignore creator bytes
     bmpImageoffset = read32(bmpFile); // Start of image data
-    Serial.print(F("Image Offset: "));
-    Serial.println(bmpImageoffset, DEC);
+    Serial.print(F("Image Offset: ")); Serial.println(bmpImageoffset, DEC);
     // Read DIB header
-    Serial.print(F("Header size: "));
-    Serial.println(read32(bmpFile));
-    bmpWidth = read32(bmpFile);
+    Serial.print(F("Header size: ")); Serial.println(read32(bmpFile));
+    bmpWidth  = read32(bmpFile);
     bmpHeight = read32(bmpFile);
-    if (read16(bmpFile) == 1)
-    {                             // # planes -- must be '1'
+    if(read16(bmpFile) == 1) { // # planes -- must be '1'
       bmpDepth = read16(bmpFile); // bits per pixel
-      Serial.print(F("Bit Depth: "));
-      Serial.println(bmpDepth);
-      if ((bmpDepth == 24) && (read32(bmpFile) == 0))
-      { // 0 = uncompressed
+      Serial.print(F("Bit Depth: ")); Serial.println(bmpDepth);
+      if((bmpDepth == 24) && (read32(bmpFile) == 0)) { // 0 = uncompressed
 
         goodBmp = true; // Supported BMP format -- proceed!
         Serial.print(F("Image size: "));
@@ -131,26 +113,22 @@ void bmpDraw(char *filename, uint8_t x, uint16_t y)
 
         // If bmpHeight is negative, image is in top-down order.
         // This is not canon but has been observed in the wild.
-        if (bmpHeight < 0)
-        {
+        if(bmpHeight < 0) {
           bmpHeight = -bmpHeight;
-          flip = false;
+          flip      = false;
         }
 
         // Crop area to be loaded
         w = bmpWidth;
         h = bmpHeight;
-        if ((x + w - 1) >= tft.width())
-          w = tft.width() - x;
-        if ((y + h - 1) >= tft.height())
-          h = tft.height() - y;
+        if((x+w-1) >= tft.width())  w = tft.width()  - x;
+        if((y+h-1) >= tft.height()) h = tft.height() - y;
 
         // Set TFT address window to clipped image bounds
         tft.startWrite(); // Start TFT transaction
         tft.setAddrWindow(x, y, w, h);
 
-        for (row = 0; row < h; row++)
-        { // For each scanline...
+        for (row=0; row<h; row++) { // For each scanline...
 
           // Seek to start of scan line.  It might seem labor-
           // intensive to be doing this on every line, but this
@@ -158,26 +136,23 @@ void bmpDraw(char *filename, uint8_t x, uint16_t y)
           // and scanline padding.  Also, the seek only takes
           // place if the file position actually needs to change
           // (avoids a lot of cluster math in SD library).
-          if (flip) // Bitmap is stored bottom-to-top order (normal BMP)
+          if(flip) // Bitmap is stored bottom-to-top order (normal BMP)
             pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
-          else // Bitmap is stored top-to-bottom
+          else     // Bitmap is stored top-to-bottom
             pos = bmpImageoffset + row * rowSize;
-          if (bmpFile.position() != pos)
-          {                 // Need seek?
+          if(bmpFile.position() != pos) { // Need seek?
             tft.endWrite(); // End TFT transaction
             bmpFile.seek(pos);
             buffidx = sizeof(sdbuffer); // Force buffer reload
-            tft.startWrite();           // Start new TFT transaction
+            tft.startWrite(); // Start new TFT transaction
           }
 
-          for (col = 0; col < w; col++)
-          { // For each pixel...
+          for (col=0; col<w; col++) { // For each pixel...
             // Time to read more pixel data?
-            if (buffidx >= sizeof(sdbuffer))
-            {                 // Indeed
+            if (buffidx >= sizeof(sdbuffer)) { // Indeed
               tft.endWrite(); // End TFT transaction
               bmpFile.read(sdbuffer, sizeof(sdbuffer));
-              buffidx = 0;      // Set index to beginning
+              buffidx = 0; // Set index to beginning
               tft.startWrite(); // Start new TFT transaction
             }
 
@@ -185,9 +160,9 @@ void bmpDraw(char *filename, uint8_t x, uint16_t y)
             b = sdbuffer[buffidx++];
             g = sdbuffer[buffidx++];
             r = sdbuffer[buffidx++];
-            tft.pushColor(tft.color565(r, g, b));
-          }             // end pixel
-        }               // end scanline
+            tft.pushColor(tft.color565(r,g,b));
+          } // end pixel
+        } // end scanline
         tft.endWrite(); // End last TFT transaction
         Serial.print(F("Loaded in "));
         Serial.print(millis() - startTime);
@@ -197,24 +172,21 @@ void bmpDraw(char *filename, uint8_t x, uint16_t y)
   }
 
   bmpFile.close();
-  if (!goodBmp)
-    Serial.println(F("BMP format not recognized."));
+  if(!goodBmp) Serial.println(F("BMP format not recognized."));
 }
 
 // These read 16- and 32-bit types from the SD card file.
 // BMP data is stored little-endian, Arduino is little-endian too.
 // May need to reverse subscript order if porting elsewhere.
 
-uint16_t read16(File &f)
-{
+uint16_t read16(File &f) {
   uint16_t result;
   ((uint8_t *)&result)[0] = f.read(); // LSB
   ((uint8_t *)&result)[1] = f.read(); // MSB
   return result;
 }
 
-uint32_t read32(File &f)
-{
+uint32_t read32(File &f) {
   uint32_t result;
   ((uint8_t *)&result)[0] = f.read(); // LSB
   ((uint8_t *)&result)[1] = f.read();
@@ -222,60 +194,3 @@ uint32_t read32(File &f)
   ((uint8_t *)&result)[3] = f.read(); // MSB
   return result;
 }
-
-/*
-
-#include "screens\randomizer_init.h"
-#include "screens\game_selection.h"
-#include "screens\player_selection.h"
-#include "screens\fighter_selection.h"
-#include "stats.h"
-
-void setup()
-{
-  Serial.begin(9600);
-
-  pinMode(pin_led_green, OUTPUT);
-  pinMode(pin_led_blue, OUTPUT);
-  pinMode(pin_button_black, INPUT_PULLUP);
-  pinMode(pin_button_joystick, INPUT_PULLUP);
-
-  lcd.begin(16, 2);
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(F("Initializing"));
-  lcd.setCursor(0, 1);
-  lcd.print(F("Entropy"));
-  initRandom();
-
-  Serial.println(F("-> Init"));
-  screen_selected = Screen::RandomizerInit;
-
-  // For testing
-  //screen_selected = Screen::PlayerSelection;
-}
-
-void loop()
-{
-  readInput();
-
-  switch (screen_selected)
-  {
-  case Screen::RandomizerInit:
-    RandomizerInitScreen();
-    break;
-
-  case Screen::GameSelection:
-    GameSelectionScreen();
-    break;
-
-  case Screen::PlayerSelection:
-    PlayerSelectionScreen();
-    break;
-
-  case Screen::FighterSelection:
-    FighterSelectionScreen();
-    break;
-  }
-}
-/**/
