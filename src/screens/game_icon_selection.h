480427b82056ec4836f7486f30bcd04d23f78cac
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-07-17 19:31:06 -0700

    sd card seem to work ok

--- src/screens/game_icon_selection.h
+++ src/screens/game_icon_selection.h
@@ -2,49 +2,41 @@
 #define GAME_ICON_SELECTION_H
 
 #include "..\tft.h"
+#include <SPI.h>
+#include <SD.h>
 
-void printDirectory(File dir, int numTabs)
-{
-    while (true)
-    {
-        File entry = dir.openNextFile();
-        if (!entry)
-        {
-            // no more files
-            break;
-        }
-        for (uint8_t i = 0; i < numTabs; i++)
-        {
-            tft.print('\t');
-        }
-        tft.print(entry.name());
-        if (entry.isDirectory())
-        {
-            tft.println("/");
-            printDirectory(entry, numTabs + 1);
-        }
-        else
-        {
-            // files have sizes, directories do not
-            tft.print("\t\t");
-            tft.println(entry.size(), DEC);
-        }
-        entry.close();
-    }
-}
+Sd2Card card;
+SdVolume volume;
+SdFile root;
 
 void GameIconSelectionScreen()
 {
     tft.println("Initializing SD card...");
-    while (SD.begin(pin_sd_ccs))
-    {
-        tft.println("failed!");
-    }
-    tft.println("OK!");
 
-    File root = SD.open("/");
-    printDirectory(root, 0);
-    tft.println("done!");
+    // we'll use the initialization code from the utility libraries
+    // since we're just testing if the card is working!
+    if (!card.init(SPI_HALF_SPEED, pin_sd_ccs))
+    {
+        tft.println("initialization failed");
+        return;
+    }
+    else
+    {
+        tft.println("Wiring is correct and a card is present.");
+    }
+
+    // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+    if (!volume.init(card))
+    {
+        tft.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+        return;
+    }
+
+    // list all files in the card with date and size
+    root.openRoot(volume);
+    root.ls(LS_R | LS_DATE | LS_SIZE);
+
+    delay(100000);
 }
 
 #endif // GAME_ICON_SELECTION_H
