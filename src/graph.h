9bd811ca8204a68996779fc10029eb2d2c27e93b
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-11-06 23:08:47 -0800

    Added sample

--- src/graph.h
+++ src/graph.h
@@ -23,6 +23,45 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(PIN_TFT_CS, PIN_TFT_DC, PIN_TFT_MOSI, PI
 #define MAP_X(x, plot, screen) (screen.xlo + (x - plot.xlo) * (screen.xhi - screen.xlo) / (plot.xhi - plot.xlo))
 #define MAP_Y(y, plot, screen) (screen.yhi - (y - plot.ylo) * (screen.yhi - screen.ylo) / (plot.yhi - plot.ylo))
 
+/*
+// Usage example
+
+void DrawTemperatureTermistor()
+{
+    double x = 0;
+    double y = ReadTemperatureTermistor();
+
+    box screen;
+    screen.xlo = 0;
+    screen.ylo = 0;
+    screen.xhi = tft.width() - 1;
+    screen.yhi = tft.height() - 1;
+
+    box plot;
+    plot.xlo = 0;
+    plot.xhi = 60;
+    plot.ylo = (int)y - 5;
+    plot.yhi = (int)y + 5;
+
+    box line;
+    line.xlo = MAP_X(x, plot, screen);
+    line.ylo = MAP_Y(y, plot, screen);
+
+    // Draw grid
+    InitializeGrid(tft, screen, plot, 10, 1, DKBLUE, WHITE, BLACK);
+    InitializeAxes(tft, screen, plot, "Temparature", "sec", "C", RED, WHITE, BLACK);
+
+    // Draw graph
+    for (double x = 1; x <= 60; x += 1)
+    {
+        line.xhi = x;
+        line.yhi = ReadTemperatureTermistor();
+        Graph(tft, screen, plot, line, GREEN);
+        delay(1000);
+    }
+}
+/**/
+
 struct box
 {
     double xlo;
