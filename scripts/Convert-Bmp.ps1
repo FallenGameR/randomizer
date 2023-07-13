e609e8aed0a9892726cc72a22515037591a57ca0
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-12 19:08:50 -0700

    Conversion of icons

---
+++ scripts/Convert-Bmp.ps1
@@ -0,0 +1,16 @@
+[Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+$dataPath = Join-Path $PsScriptRoot "..\data"
+
+foreach( $icon in ls $dataPath -Recurse -Include *.bmp )
+{
+    $convertFile = New-Object System.Drawing.Bitmap($iconFile)
+    $newFile = $convertFile.Clone(
+        [System.Drawing.Rectangle]::FromLTRB(0, 0, $convertFile.Width, $convertFile.Height),
+        [System.Drawing.Imaging.PixelFormat]::Format24bppRgb)
+    $newFile.Save("$iconFile.tmp", [System.Drawing.Imaging.ImageFormat]::Bmp)
+    $newFile.Dispose()
+    $convertFile.Dispose()
+    Remove-Item $iconFile
+    Rename-Item "$iconFile.tmp" "$iconFile"
+}
