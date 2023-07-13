488aae37905906772fda01fb9b2db6579d8b9967
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-12 23:13:07 -0700

    format main

--- src/fighters.h
+++ src/fighters.h
@@ -16,4 +16,6 @@ byte fighter_index_second3 = NO_FIGHTER;
 // Number of fighters in the current game
 byte n_fighters = 0;
 
+byte test[100];
+
 #endif // FIGHTERS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-//#define DEBUG
+// #define DEBUG
 
 #include "screens\randomizer_init.h"
 #include "screens\game_selection.h"
@@ -14,77 +14,78 @@
 
 void setup()
 {
-  Serial.begin(9600);
 
-  initSd();
-  InitPlayerColors();
+    Serial.begin(9600);
 
-  n_games = readNumberOfGames();
-  Serial.print("Number of games: ");
-  Serial.println(n_games);
+    initSd();
+    InitPlayerColors();
 
-  n_players = readNumberOfPlayers();
-  Serial.print("Number of players: ");
-  Serial.println(n_players);
+    n_games = readNumberOfGames();
+    Serial.print("Number of games: ");
+    Serial.println(n_games);
 
-  pinMode(pin_led_green, OUTPUT);
-  pinMode(pin_led_blue, OUTPUT);
-  pinMode(pin_button_black, INPUT_PULLUP);
-  pinMode(pin_button_joystick, INPUT_PULLUP);
+    n_players = readNumberOfPlayers();
+    Serial.print("Number of players: ");
+    Serial.println(n_players);
 
-  tft.reset();
-  uint16_t identifier = tft.readID();
-  tft.begin(identifier);
+    pinMode(pin_led_green, OUTPUT);
+    pinMode(pin_led_blue, OUTPUT);
+    pinMode(pin_button_black, INPUT_PULLUP);
+    pinMode(pin_button_joystick, INPUT_PULLUP);
 
-  tft.setRotation(1);
-  tft.fillScreen(BLACK);
-  tft.setCursor(0, 0);
-  tft.setTextSize(FONT_SIZE);
-  tft.setTextColor(WHITE, BLACK);
-  tft.print(F("Initializing "));
-  tft.println(F("Entropy"));
+    tft.reset();
+    uint16_t identifier = tft.readID();
+    tft.begin(identifier);
 
-  initRandom();
+    tft.setRotation(1);
+    tft.fillScreen(BLACK);
+    tft.setCursor(0, 0);
+    tft.setTextSize(FONT_SIZE);
+    tft.setTextColor(WHITE, BLACK);
+    tft.print(F("Initializing "));
+    tft.println(F("Entropy"));
 
-  Serial.println(F("-> Init"));
-  screen_selected = Screen::RandomizerInit;
+    initRandom();
 
-  // For testing
-  // screen_selected = Screen::Tester;
+    Serial.println(F("-> Init"));
+    screen_selected = Screen::RandomizerInit;
+
+    // For testing
+    // screen_selected = Screen::Tester;
 }
 
 void loop()
 {
-  readInput();
+    readInput();
 
-  switch (screen_selected)
-  {
-  case Screen::RandomizerInit:
-    RandomizerInitScreen();
-    break;
+    switch (screen_selected)
+    {
+    case Screen::RandomizerInit:
+        RandomizerInitScreen();
+        break;
 
-  case Screen::GameIconSelection:
-    GameIconSelectionScreen();
-    break;
+    case Screen::GameIconSelection:
+        GameIconSelectionScreen();
+        break;
 
-  case Screen::GameSelection:
-    GameSelectionScreen();
-    break;
+    case Screen::GameSelection:
+        GameSelectionScreen();
+        break;
 
-  case Screen::PlayerSelection:
-    PlayerSelectionScreen();
-    break;
+    case Screen::PlayerSelection:
+        PlayerSelectionScreen();
+        break;
 
-  case Screen::FighterSelection:
-    FighterSelectionScreen();
-    break;
+    case Screen::FighterSelection:
+        FighterSelectionScreen();
+        break;
 
-  case Screen::TotalsShow:
-    TotalShowScreen();
-    break;
+    case Screen::TotalsShow:
+        TotalShowScreen();
+        break;
 
-  case Screen::Tester:
-    TesterScreen();
-    break;
-  }
+    case Screen::Tester:
+        TesterScreen();
+        break;
+    }
 }
