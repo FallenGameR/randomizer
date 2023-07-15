cceb51f83569d339262220798cf075c0d911c253
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-15 16:44:38 -0700

    Tester works somehow

--- src/screens/tester.h
+++ src/screens/tester.h
@@ -63,6 +63,66 @@ void TesterScreen()
     // MockRecordMatchOutcome();
     // RenderTotals();
     // delay(60000);
+
+    if (screen_redraw)
+    {
+        SelectGame(game_index);
+        tft.fillScreen(GREEN);
+        tft.setCursor(0, 0);
+        tft.setTextSize(FONT_SIZE);
+        tft.setTextColor(WHITE, BLACK);
+
+        PRINT2_B(setGameName(game_index), tft, Serial);
+        Serial.println();
+
+        if (isTagGame)
+        {
+            tft.println();
+            tft.print(F("Tag"));
+            Serial.println(F("Tag"));
+        }
+
+        screen_redraw = false;
+    }
+
+    if (input_allowed)
+    {
+        if (X_RIGHT)
+        {
+            game_index = (game_index + n_games + 1) % n_games;
+            SelectGame(game_index);
+            input_allowed = false;
+            screen_redraw = true;
+        }
+
+        if (X_LEFT)
+        {
+            game_index = (game_index + n_games - 1) % n_games;
+            SelectGame(game_index);
+            input_allowed = false;
+            screen_redraw = true;
+        }
+
+        if (Y_UP || Y_DOWN)
+        {
+            if (game_tag > 0)
+            {
+                isTagGame = !isTagGame;
+            }
+
+            input_allowed = false;
+            screen_redraw = true;
+        }
+
+        if (BUTTON_BLACK)
+        {
+            Serial.print(F("-> Match "));
+            Serial.println(match_current + 1);
+            screen_selected = Screen::PlayerSelection;
+            input_allowed = false;
+            screen_redraw = true;
+        }
+    }
 }
 
 #endif // TESTER_H
