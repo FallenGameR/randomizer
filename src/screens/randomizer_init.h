92eaf62a5cfaae8250b6a6e318911bae828b04d6
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-20 21:05:27 -0700

    cleanup

--- src/screens/randomizer_init.h
+++ src/screens/randomizer_init.h
@@ -56,86 +56,15 @@ struct init_setting init_settings[] = {
 // Settings table length
 #define SETTINGS_LENGTH 13
 
-// Redraw games value or error when games could not be read from SD card
-void InitDrawGamesValue(int new_value)
-{
-    init_setting* entry = &init_settings[SETTING_GAMES_IDX];
-
-    // Clear previous value
-    tft.setCursor(entry->col * CHAR_WIDTH, entry->row * CHAR_HEIGHT);
-    tft.setTextColor(BLACK, BLACK);
-    if( entry->value != 0 )
-    {
-        tft.print(entry->value);
-    }
-    else
-    {
-        // Erasing "SD card error" that is hard to place into PROGMEM
-        for( size_t i = 0; i < 13; i++ )
-        {
-            tft.print(' ');
-        }
-    }
-
-    // Set new value
-    Serial.print(F("Games: "));
-    tft.setCursor(entry->col * CHAR_WIDTH, entry->row * CHAR_HEIGHT);
-    tft.setTextColor(WHITE, BLACK);
-    if( new_value != 0 )
-    {
-        tft.println(new_value);
-        Serial.println(new_value);
-    }
-    else
-    {
-        tft.println(F("SD card error"));
-        Serial.println(F("SD card error"));
-    }
-
-    // Update internal state
-    entry->value = new_value;
-}
-
-// Redraw an integer value
-void InitRedrawIntValue(size_t index, int direction)
-{
-    // Find the new value
-    init_setting* entry = &init_settings[index];
-    int new_value = entry->value + direction;
-
-    // Clear previous value
-    tft.setCursor(entry->col * CHAR_WIDTH, entry->row * CHAR_HEIGHT);
-    tft.setTextColor(BLACK, BLACK);
-    tft.print(entry->value);
-
-    // Set new value
-    tft.setCursor(entry->col * CHAR_WIDTH, entry->row * CHAR_HEIGHT);
-    tft.setTextColor(WHITE, BLACK);
-    tft.println(new_value);
-
-    // Dump to serial
-    const __FlashStringHelper *serial_title = F("");
-    switch (index)
-    {
-        case SETTING_SEED_IDX: serial_title = F("Seed: "); break;
-        case SETTING_FAIRNESS_IDX: serial_title = F("Fair: "); break;
-    }
-    Serial.print(serial_title);
-    Serial.println(new_value);
-
-    // Update internal state
-    entry->value = new_value;
-}
-
-// Redraw cursor
-void InitRedrawCursor(int direction)
+// Update and redraw cursor that moves between selectable settings
+void UpdateCursorPosition(int movement)
 {
     // Find the new index
     size_t new_index = cursor_index;
     init_setting* new_entry;
     do
     {
-        new_index = (new_index + direction + SETTINGS_LENGTH) % SETTINGS_LENGTH;
+        new_index = (new_index + movement + SETTINGS_LENGTH) % SETTINGS_LENGTH;
         new_entry = &init_settings[new_index];
     }
     while( !new_entry->is_selectable );
@@ -165,8 +94,80 @@ void InitRedrawCursor(int direction)
     cursor_index = new_index;
 }
 
-// Toggle a bool value
-void InitRedrawBoolValue()
+// Update and redraw games setting
+void UpdateGamesSetting(int new_value)
+{
+    init_setting* entry = &init_settings[SETTING_GAMES_IDX];
+
+    // Clear previous value
+    tft.setCursor(entry->col * CHAR_WIDTH, entry->row * CHAR_HEIGHT);
+    tft.setTextColor(BLACK, BLACK);
+    if( entry->value != 0 )
+    {
+        tft.print(entry->value);
+    }
+    else
+    {
+        // Erasing "SD card error" string that is tricky place into PROGMEM for reuse
+        for( size_t i = 0; i < 13; i++ )
+        {
+            tft.print(' ');
+        }
+    }
+
+    // Set new value
+    Serial.print(F("Games: "));
+    tft.setCursor(entry->col * CHAR_WIDTH, entry->row * CHAR_HEIGHT);
+    tft.setTextColor(WHITE, BLACK);
+    if( new_value != 0 )
+    {
+        tft.println(new_value);
+        Serial.println(new_value);
+    }
+    else
+    {
+        tft.println(F("SD card error"));
+        Serial.println(F("SD card error"));
+    }
+
+    // Update internal state
+    entry->value = new_value;
+}
+
+// Update and redraw an integer setting
+void UpdateIntegerSetting(size_t index, int increment)
+{
+    // Find the new value
+    init_setting* entry = &init_settings[index];
+    int new_value = entry->value + increment;
+
+    // Clear previous value
+    tft.setCursor(entry->col * CHAR_WIDTH, entry->row * CHAR_HEIGHT);
+    tft.setTextColor(BLACK, BLACK);
+    tft.print(entry->value);
+
+    // Set new value
+    tft.setCursor(entry->col * CHAR_WIDTH, entry->row * CHAR_HEIGHT);
+    tft.setTextColor(WHITE, BLACK);
+    tft.println(new_value);
+
+    // Dump to serial
+    const __FlashStringHelper *serial_title = F("");
+    switch (index)
+    {
+        case SETTING_SEED_IDX: serial_title = F("Seed: "); break;
+        case SETTING_FAIRNESS_IDX: serial_title = F("Fair: "); break;
+    }
+    Serial.print(serial_title);
+    Serial.println(new_value);
+
+    // Update internal state
+    entry->value = new_value;
+}
+
+// Update and redraw a bool switch setting that the cursor is currently on
+// It is caller's responsibility to make sure cursor is standing on a switch
+void UpdateCurrentlySelectedSwitchSetting()
 {
     // Toggle the value
     init_setting* entry = &init_settings[cursor_index];
@@ -184,8 +185,10 @@ void InitRedrawBoolValue()
     Serial.println(entry->value);
 }
 
+// Entry function of the init screen
 void RandomizerInitScreen()
 {
+    // Full settings reinit and redraw of the screen
     if (screen_redraw)
     {
         tft.fillScreen(BLACK);
@@ -202,20 +205,20 @@ void RandomizerInitScreen()
             n_players = readNumberOfPlayers();
 
             tft.print(F("  Games: "));
-            InitDrawGamesValue(n_games);
+            UpdateGamesSetting(n_games);
         }
 
         initRandom();
         {
             tft.print(F("  Seed:  "));
             (&init_settings[SETTING_SEED_IDX])->value = random_seed;
-            InitRedrawIntValue(SETTING_SEED_IDX, 0);
+            UpdateIntegerSetting(SETTING_SEED_IDX, 0);
 
             tft.print(F("  Fair:  "));
             random_fairness = n_players * (n_players - 1);
             random_fairness_increment = random_fairness;
             (&init_settings[SETTING_FAIRNESS_IDX])->value = random_fairness;
-            InitRedrawIntValue(SETTING_FAIRNESS_IDX, 0);
+            UpdateIntegerSetting(SETTING_FAIRNESS_IDX, 0);
         }
 
         tft.println();
@@ -241,11 +244,12 @@ void RandomizerInitScreen()
         }
 
         cursor_index = 1;
-        InitRedrawCursor(0);
+        UpdateCursorPosition(0);
 
         screen_redraw = false;
     }
 
+    // Start processing of the input controls
     if (input_allowed)
     {
         // Soft reset to quickly fix SD reader and screen glitches
@@ -274,7 +278,7 @@ void RandomizerInitScreen()
         if (Y_UP || Y_DOWN)
         {
             int direction = Y_UP ? -1 : +1;
-            InitRedrawCursor(direction);
+            UpdateCursorPosition(direction);
             input_allowed = false;
         }
 
@@ -287,7 +291,7 @@ void RandomizerInitScreen()
             {
                 case SETTING_SEED_IDX:
                 {
-                    InitRedrawIntValue(SETTING_SEED_IDX, direction);
+                    UpdateIntegerSetting(SETTING_SEED_IDX, direction);
                     // input_allowed stays the same, seed can be changed quickly
                     break;
                 }
@@ -297,13 +301,13 @@ void RandomizerInitScreen()
                     int new_fairness = old_fairness + random_fairness_increment * direction;
                     if( new_fairness <= 0 ) { new_fairness += random_fairness_increment; }
                     if( new_fairness > 255 ) { new_fairness -= random_fairness_increment; }
-                    InitRedrawIntValue(SETTING_FAIRNESS_IDX, (new_fairness - old_fairness));
+                    UpdateIntegerSetting(SETTING_FAIRNESS_IDX, (new_fairness - old_fairness));
                     input_allowed = false;
                     break;
                 }
                 default:
                 {
-                    InitRedrawBoolValue();
+                    UpdateCurrentlySelectedSwitchSetting();
                     input_allowed = false;
                     break;
                 }
