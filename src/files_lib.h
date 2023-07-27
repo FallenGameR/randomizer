8f88c51892c32c4fd72a7125fe514c6619d97190
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-26 23:09:53 -0700

    done for today

--- src/files_lib.h
+++ src/files_lib.h
@@ -56,10 +56,10 @@ File openFighterFolder(byte game_index, byte fighter_index);
 
 // Set b_path to /games/<game_name_from_index>/<progmem_path>
 // progmem_path should be a path constant stored in PROGMEM
-char* setGameRelativePathBuffer(byte gameIndex, const char *progmem_path);
+char* setGameRelativePathBuffer(byte game_index, const char *progmem_path);
 
 // Set b_path to /games/<game_name_from_index>/fighters/<fighter_name_from_index>/<progmem_path>
 // progmem_path should be a path constant stored in PROGMEM
-char* setFighterRelativePathBuffer(byte gameIndex, byte fighterIndex, const char *path);
+char* setFighterRelativePathBuffer(byte game_index, byte fighter_index, const char *progmem_path);
 
 #endif // FILES_LIB_H
