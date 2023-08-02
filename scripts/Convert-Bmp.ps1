2ee72644b068fac93df99c269073d3df3245759a
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-08-01 20:58:09 -0700

    correct convert script

--- scripts/Convert-Bmp.ps1
+++ scripts/Convert-Bmp.ps1
@@ -4,19 +4,21 @@ $dataPath = Join-Path $PsScriptRoot "..\data"
 
 function Convert-Bmp( $file )
 {
-    $path = if( $file -is [IO.FileInfo] ) { $path.FullName } else { $file }
+    $path = if( $file -is [IO.FileInfo] ) { $file.FullName } else { $file }
     $convertFile = New-Object System.Drawing.Bitmap($path)
     $newFile = $convertFile.Clone(
         [System.Drawing.Rectangle]::FromLTRB(0, 0, $convertFile.Width, $convertFile.Height),
         [System.Drawing.Imaging.PixelFormat]::Format24bppRgb)
-    $newFile.Save("$iconFile.tmp", [System.Drawing.Imaging.ImageFormat]::Bmp)
+    $newFile.Save("$path.tmp", [System.Drawing.Imaging.ImageFormat]::Bmp)
     $newFile.Dispose()
     $convertFile.Dispose()
-    Remove-Item $iconFile
-    Rename-Item "$iconFile.tmp" "$iconFile"
+    Remove-Item $path
+    Rename-Item "$path.tmp" $path
 }
 
-foreach( $iconFile in ls $dataPath -Recurse -Include *.bmp )
+$files = ls $dataPath -Recurse -Include *.bmp
+$files = ls data\games\*\icon.bmp
+foreach( $iconFile in $files )
 {
     Convert-Bmp $iconFile
 }
