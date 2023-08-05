679ddfe8c3c52f3a18472fb92e8fd10484955b98
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-08-05 15:04:42 -0700

    04 screen done

---
+++ src/screens/04_totals_show_screen.cpp
@@ -0,0 +1,35 @@
+#include "04_totals_show_screen.h"
+#include "..\screens.h"
+#include "..\totals.h"
+#include "..\input.h"
+
+void TotalShowScreen()
+{
+    if (screen_redraw)
+    {
+        RenderTotals();
+        DumpStats();
+        screen_redraw = false;
+    }
+
+    if (input_allowed)
+    {
+        // Black button returns to games selection with retaining the history
+        if (BUTTON_BLACK)
+        {
+            Serial.println(F("-> Game"));
+            screen_selected = Screen::GameIconSelection;
+            input_allowed = false;
+            screen_redraw = true;
+        }
+
+        // Soft reset to do a soft reset
+        if (BUTTON_JOYSTICK)
+        {
+            Serial.println(F("-> Init"));
+            screen_selected = Screen::RandomizerInit;
+            input_allowed = false;
+            screen_redraw = true;
+        }
+    }
+}
--- src/screens/04_totals_show_screen.h
+++ src/screens/04_totals_show_screen.h
@@ -1,38 +1,6 @@
 #ifndef TOTALS_SHOW_H
 #define TOTALS_SHOW_H
 
-#include "..\screens.h"
-#include "..\totals.h"
-
-void TotalShowScreen()
-{
-    if (screen_redraw)
-    {
-        RenderTotals();
-        DumpStats();
-        screen_redraw = false;
-    }
-
-    if (input_allowed)
-    {
-        // Black button returns to games selection with retaining the history
-        if (BUTTON_BLACK)
-        {
-            Serial.println(F("-> Game"));
-            screen_selected = Screen::GameIconSelection;
-            input_allowed = false;
-            screen_redraw = true;
-        }
-
-        // Soft reset to do a soft reset
-        if (BUTTON_JOYSTICK)
-        {
-            Serial.println(F("-> Init"));
-            screen_selected = Screen::RandomizerInit;
-            input_allowed = false;
-            screen_redraw = true;
-        }
-    }
-}
+void TotalShowScreen();
 
 #endif // TOTALS_SHOW_H
