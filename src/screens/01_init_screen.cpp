ef8570cf2cbb0c992cdb9d3dea961780dc5ffd35
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-08-05 15:14:47 -0700

    init screen cleanup

--- src/screens/01_init_screen.cpp
+++ src/screens/01_init_screen.cpp
@@ -1,7 +1,50 @@
 #include "01_init_screen.h"
 
+#include <Arduino.h>
+
+#include "..\screens.h"
+#include "..\random.h"
+#include "..\input.h"
+#include "..\players.h"
+#include "..\tft.h"
+#include "..\stats.h"
+#include "..\colors.h"
+
+// Indexes of various settings in the table
+#define SETTING_GAMES_IDX 0
+#define SETTING_SEED_IDX 1
+#define SETTING_FAIRNESS_IDX 2
+#define SETTING_PLAYER_NUMBER_IDX 3
+#define SETTING_PLAYER_FIRST_IDX 4
+#define SETTING_PLAYER_LAST_IDX (SETTING_PLAYER_FIRST_IDX + MAX_PLAYERS)
+
+// Settings table length
+#define SETTINGS_LENGTH 13
+
+// Combination of different player pairs
+#define MIN_FAIRNESS (init_settings[SETTING_PLAYER_NUMBER_IDX].value * (init_settings[SETTING_PLAYER_NUMBER_IDX].value - 1))
+
+// Index of the currently selected entry in the table on the current screen
 size_t cursor_index = 0;
 
+// Structure that describes setting that can be edited on the init screen
+struct init_setting
+{
+    // Row on screen, starts with 0 from top
+    int16_t row;
+
+    // Column on screen, starts with 0 from LEFT
+    int16_t col;
+
+    // Can this row be selected for edit
+    bool is_selectable;
+
+    // Currently displayed value, how exactly to
+    // render it depends on the data it represents
+    int value;
+};
+
+// Table of editable settings on the init screen
 struct init_setting init_settings[] = {
     // r, c, sel,   val
     {  3, 9, false, 0 }, // Games
@@ -20,6 +63,7 @@ struct init_setting init_settings[] = {
     { 19, 2, false, 0 }, // Player 9
 };
 
+// Update and redraw cursor that moves between selectable settings
 void UpdateInitCursorPosition(int movement)
 {
     // Find the new index
@@ -57,6 +101,7 @@ void UpdateInitCursorPosition(int movement)
     cursor_index = new_index;
 }
 
+// Update and redraw games setting
 void UpdateGamesSetting(int new_value)
 {
     init_setting* entry = &init_settings[SETTING_GAMES_IDX];
@@ -96,6 +141,7 @@ void UpdateGamesSetting(int new_value)
     entry->value = new_value;
 }
 
+// Update and redraw an integer setting
 void UpdateIntegerSetting(size_t index, int increment)
 {
     // Find the new value
@@ -128,6 +174,8 @@ void UpdateIntegerSetting(size_t index, int increment)
     entry->value = new_value;
 }
 
+// Update and redraw a bool switch setting that the cursor is currently on
+// It is caller's responsibility to make sure cursor is standing on a switch
 void UpdateCurrentlySelectedSwitchSetting()
 {
     // Toggle the value
--- src/screens/01_init_screen.h
+++ src/screens/01_init_screen.h
@@ -1,64 +1,6 @@
 #ifndef RANDOMIZER_INIT_H
 #define RANDOMIZER_INIT_H
 
-#include "..\screens.h"
-#include "..\random.h"
-#include "..\input.h"
-#include "..\players.h"
-#include "..\tft.h"
-#include "..\stats.h"
-#include "..\colors.h"
-
-// Index of the currently selected entry in the table on the current screen
-extern size_t cursor_index;
-
-// Structure that describes setting that can be edited on the init screen
-struct init_setting
-{
-    // Row on screen, starts with 0 from top
-    int16_t row;
-
-    // Column on screen, starts with 0 from LEFT
-    int16_t col;
-
-    // Can this row be selected for edit
-    bool is_selectable;
-
-    // Currently displayed value, how exactly to
-    // render it depends on the data it represents
-    int value;
-};
-
-// Table of editable settings on the init screen
-extern struct init_setting init_settings[];
-
-// Indexes of various settings in the table
-#define SETTING_GAMES_IDX 0
-#define SETTING_SEED_IDX 1
-#define SETTING_FAIRNESS_IDX 2
-#define SETTING_PLAYER_NUMBER_IDX 3
-#define SETTING_PLAYER_FIRST_IDX 4
-#define SETTING_PLAYER_LAST_IDX (SETTING_PLAYER_FIRST_IDX + MAX_PLAYERS)
-
-// Settings table length
-#define SETTINGS_LENGTH 13
-
-// Combination of different player pairs
-#define MIN_FAIRNESS (init_settings[SETTING_PLAYER_NUMBER_IDX].value * (init_settings[SETTING_PLAYER_NUMBER_IDX].value - 1))
-
-// Update and redraw cursor that moves between selectable settings
-void UpdateInitCursorPosition(int movement);
-
-// Update and redraw games setting
-void UpdateGamesSetting(int new_value);
-
-// Update and redraw an integer setting
-void UpdateIntegerSetting(size_t index, int increment);
-
-// Update and redraw a bool switch setting that the cursor is currently on
-// It is caller's responsibility to make sure cursor is standing on a switch
-void UpdateCurrentlySelectedSwitchSetting();
-
 // Entry function of the init screen
 void RandomizerInitScreen();
 
