4b7273aeaf968af4c03bf4febfe4be03e1245922
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-08-05 14:33:27 -0700

    fighters extracted

--- src/fighters.h
+++ src/fighters.h
@@ -1,6 +1,8 @@
 #ifndef FIGHTERS_H
 #define FIGHTERS_H
 
+#include <Arduino.h>
+
 // Placeholder for empty fighter index
 #define NO_FIGHTER 255
 
@@ -8,32 +10,32 @@
 #define MAX_FIGHTERS_IN_GAME 64
 
 // For regular matches
-byte fighter_index_first = NO_FIGHTER;
-byte fighter_index_second = NO_FIGHTER;
+extern byte fighter_index_first;
+extern byte fighter_index_second;
 
 // For tag matches
-byte fighter_index_first2 = NO_FIGHTER;
-byte fighter_index_second2 = NO_FIGHTER;
-byte fighter_index_first3 = NO_FIGHTER;
-byte fighter_index_second3 = NO_FIGHTER;
+extern byte fighter_index_first2;
+extern byte fighter_index_second2;
+extern byte fighter_index_first3;
+extern byte fighter_index_second3;
 
 // Number of fighters in the current game
-byte n_fighters = 0;
+extern byte n_fighters;
 
 // We randomize fighters in a way that every fighter is used at least once before repeating
 // But at the same time it is ok for the same fighter to be present on both sides
 // This logic maximizes the number of unique fighters in the game session while allowing for twin matches
 
 // Randomized fighters for the left seat
-byte fighters_left[MAX_FIGHTERS_IN_GAME];
+extern byte fighters_left[];
 
 // Randomized fighters for the right seat
-byte fighters_right[MAX_FIGHTERS_IN_GAME];
+extern byte fighters_right[];
 
 // Current position in the fighters on the left seat array
-size_t fighters_left_position = 0;
+extern size_t fighters_left_position;
 
 // Current position in the fighters on the right seat array
-size_t fighters_right_position = 0;
+extern size_t fighters_right_position;
 
 #endif // FIGHTERS_H
